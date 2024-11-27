b867836ed0a79091a7cbc00a163f98e21a6f287b
“ElisiusKLP” <elisius@live.dk>
2024-11-27 23:25:11 +0100

    working version of ModelComparisonNode

--- model_testing/testing.jl
+++ model_testing/testing.jl
@@ -1,4 +1,5 @@
 
+#using ActionModels
 include("/work/HGF_MOD/HierarchicalGaussianFiltering.jl/src/HierarchicalGaussianFiltering.jl")
 using .HierarchicalGaussianFiltering
 
@@ -55,11 +56,16 @@ edges_sensory = Dict(
 )
 
 #CHANGE THIS TO THE CORRECT ORDER
+# TODO Right now the model comparison is treated as outside the ordered_nodes
+# because the model_comparison always happens at a fixed time after input nodes, before state updates
+
 update_order_sensory = ["u_A", "u_V", "Seg_sA", "Seg_sV", "FF_sAV"]
 
+#=
 families_dict = Dict{String, Tuple{Vararg{String}}}()
 families_dict["Segregation"] = ("Seg_sA", "Seg_sV")
 families_dict["ForcedFusion"] = ("FF_sAV",)
+=#
 
 families_dict = Dict(
     "Segregation" => ["Seg_sA", "Seg_sV", "u_A", "u_V"],
@@ -73,8 +79,11 @@ hgf_sensory = init_hgf(
     families = families_dict
 )
 
+########################################
 ### TEST Initialization of Structure ###
+########################################
 
+#=
 hgf_sensory.all_nodes
 hgf_sensory.all_nodes.families
 hgf_sensory.input_nodes
@@ -123,8 +132,57 @@ hgf_sensory.all_nodes["Seg_sV"].edges
 
 println(fieldnames(typeof(hgf_sensory.all_nodes["Seg_sV"].edges)))
 
+hgf_sensory.state_nodes
+=#
+
+hgf_sensory.all_nodes["u_A"].parameters.coupling_strengths
+
+hgf_sensory.all_nodes["FF_sAV"].edges.observation_children
+
+###################
 ### Give INPUTS ###
+###################
 
 # Load input data
 
-# Feed inputs
+# changed timstep to range 0 to 1000
+file_path = "/work/HGF_MOD/model_testing/input_data/data_sim3_2024-10-23_111804.csv"
+
+# import data
+using CSV
+using DataFrames
+
+# Load the CSV file into a DataFrame
+df = CSV.read(file_path, DataFrame)
+
+
+# Convert all column names to lowercase
+rename!(df, Dict(col => lowercase(col) for col in names(df)))
+
+println("First few rows of the DataFrame:")
+println(first(df, 5))
+print(names(df))
+
+
+# Create the vector using comprehension
+input_vector =[]
+input_vector = [ [row.auditory_position, row.visual_position, row.auditory_position, row.visual_position] for row in eachrow(df) ]
+
+
+### Feed inputs ###
+
+# cut down on input length
+input_vector = input_vector[1:1000]
+
+reset!(hgf_sensory)
+
+typeof(hgf_sensory)
+hgf_sensory.all_nodes
+
+give_inputs!(hgf_sensory, input_vector)
+
+history = get_history(hgf_sensory)
+
+for key in keys(history)
+	println("this is the key $key")
+end
